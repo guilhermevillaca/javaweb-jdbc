3e923148c3628a525359fcaee967c46baca0c0c8
Guilherme Villaca <guidvillaca@gmail.com>
2025-06-09 19:57:51 -0300

    Update PessoaDAO.java com postgresql

--- src/java/br/com/villaca/dao/PessoaDAO.java
+++ src/java/br/com/villaca/dao/PessoaDAO.java
@@ -34,3 +34,43 @@ public class PessoaDAO {
     }
 
 }
+
+
+/*
+
+
+package br.com.villaca.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import br.com.villaca.model.Pessoa;
+
+public class PessoaDAO {
+
+    private final String url = "jdbc:postgresql://localhost:5432/javaweb1"; // PostgreSQL usa 'postgresql' e porta 5432
+    private final String user = "postgres"; // seu usuário do PostgreSQL
+    private final String password = "postgres"; // sua senha do PostgreSQL
+
+    public void salvar(Pessoa pessoa) {
+        String sql = "INSERT INTO pessoa (nome, email, telefone) VALUES (?, ?, ?)";
+
+        try {
+            Class.forName("org.postgresql.Driver"); // driver PostgreSQL
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, pessoa.getNome());
+            preparedStatement.setString(2, pessoa.getEmail());
+            preparedStatement.setString(3, pessoa.getTelefone());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+*/
